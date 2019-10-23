1122d4f5e0359be0861b574fb749d393baa164a9
haticeakyel <56586646+haticeakyel@users.noreply.github.com>
2019-10-23 12:22:02 +0300

    add document for menu.

--- atm_banking/main.c
+++ atm_banking/main.c
@@ -9,7 +9,70 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    /*
+        authenticate olabilmek icin 1 fonksiyon'a ihtiyacımız var, 1 numaralı menu o işe yarıyor
+     
+        1- Insert your card
+        
+        bu menu seçilince kullanıcıya müşteri numarası sorulur ve şifresi sorulur.
+     
+        oluşturulcak method
+     
+        - int authenticate(account_number, password)
+     
+        eğer başarılı giriş yaparsa authenticate methodu geriye pozitif bir sayı döncek.
+        eğer başarılı giriş yapamazsa authenticate methodu geriye negatif bir sayı döncek.
+    */
+    
+    /*
+        authenticate olduktan sonra kullanıcıya yapabileceği işlemleri göstermeli.
+     
+        1- Account information
+        2- Transactions
+        3- Deposit
+        4- Pin change
+        5- Balance
+        6- Withdrawal
+        7- Bill payment
+     
+        Kullanıcı 1. seçeneği seçerse;
+            + Hesap numarası
+            + Şube bilgisi
+            + Bakiyesi
+     
+        Kullanıcı 2. seçeneği seçerse;
+            + Kullanıcının bizim atm'yi kullanarak yaptığı işlemler kayıt altına alıncak.
+            + Bu seçenek altında yapılan işlemler gösterilcek.
+     
+        Kullanıcı 3. seçeneği seçerse;
+            + Kullanıcıya hangi çeşit hesabına para yatırmak istediği sorulur. (Dolar, TL, Euro)
+                + Ne kadar miktar para yatıracağı sorulur.
+                + Dekont gösterilir.
+                    + Hesap bilgileri gösterilir.
+                    + Ne kadar miktar para yatırdığı gösterilir.
+        
+        Kullanıcı 4. seçeneği seçerse;
+            + Kullanıcıdan değiştirmek istediği pin numarası alınır.
+            + Pin numarasını doğrulamak için tekrardan aynı pin numarasını girmesi istenir.
+            + Pin numaraları birbirlerine eşitse pin değiştirilir.
+     
+        Kullanıcı 5. seçeneği seçerse;
+            + Kullanıcının bakiyesi gösterilir.
+     
+        Kullanıcı 6. seçeneği seçerse;
+            + Kullanıcıya önceden belirlenmiş miktarlar gösterilir. 10 hariç 10,un katları. (20, 50, 100)
+            + Seçeneklerden birini seçmezse kendisi giriş yapabilir. Yine 10'un katı olmak zorunda.
+     
+        Kullanıcı 7. seçeneği seçerse;
+            + Hangi tip faturayı ödemek istediği sorulur.
+            + Tutar random belirlenir.
+            + Kullanıcı onay verdikten sonra, hesabından fatura tutarı düşülür.
+            + Dekont verilir.
+                + Faturanın adı, tipi.
+                + Hesap bilgileri.
+                + Ne kadar ödediği.
+     
+     */
     printf("Hello, from the other side. \n hello");
     return 0;
 }
